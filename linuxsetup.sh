2f38ebbf7ae093dee4ce67b8dd322de459b03a58
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2019-08-10 19:10:59 -0400

    Create linuxsetup.sh

---
+++ linuxsetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+apt-get update -y
+apt update -y
+apt install vlc -y
+apt install python -y
+apt install python3 -y
+alias python=python3
+apt install python-pip -y
+apt install python3-pip -y
+alias pip=pip3
+apt-get install r-base -y
+apt-get install r-base-dev -y
+apt-get install exfat-fuse exfat-utils -y
