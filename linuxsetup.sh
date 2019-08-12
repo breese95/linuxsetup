5922d0c0486d0013fb103c023c99837b06343359
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2019-08-12 09:10:24 -0400

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -2,6 +2,7 @@
 
 apt-get update -y
 apt update -y
+apt upgrade -y
 apt install vlc -y
 apt install python -y
 apt install python3 -y
@@ -9,6 +10,10 @@ alias python=python3
 apt install python-pip -y
 apt install python3-pip -y
 alias pip=pip3
+pip install --upgrade pip -y
+alias pip=pip3
 apt-get install r-base -y
 apt-get install r-base-dev -y
 apt-get install exfat-fuse exfat-utils -y
+#SET VLC AS DEFAULT AUDIO AND DEFAULT VIDEO
+#DOWNLOAD AND INSTALL VSCODE, RSTUDIO, JAVA RUNTIME ENVIRONEMNET, JAVA DEVELOPMENT KIT, NETBEANS, APACHE XAMPP
