8c6964f8590b2609e74be4819962eb04b33430ce
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2019-08-13 14:09:11 -0400

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -15,9 +15,11 @@ alias pip=pip3
 pip install --upgrade pip
 alias pip=pip3
 apt install docker.io -y
+apt-get install php -y
 apt-get install r-base -y
 apt-get install r-base-dev -y
-apt-get install php -y
+apt install default-jre -y
+apt install default-jdk -y
 apt install snapd -y
 export PATH="$PATH:/snap/bin"
 snap install go --classic
@@ -30,5 +32,31 @@ code --install-extension ms-azuretools.vscode-docker --force --user-data-dir
 code --install-extension redhat.java --force --user-data-dir
 code --install-extension octref.vetur --force --user-data-dir
 code --install-extension ms-vscode.go --force --user-data-dir
-#SET VLC AS DEFAULT AUDIO AND DEFAULT VIDEO
-#DOWNLOAD AND INSTALL RSTUDIO, JAVA RUNTIME ENVIRONEMNET, JAVA DEVELOPMENT KIT, NETBEANS, APACHE XAMPP
+echo ""
+echo "PROGRAMS INSTALLED..."
+echo "Visual Studio Code"
+echo "VLC media player"
+echo "Docker"
+echo ""
+echo "PROGRAMMING LANGUAGES INSTALLED..."
+echo "Python"
+python --version
+echo "Python3"
+python3 --version
+echo "PHP"
+php -v
+echo "R"
+R --version
+echo "Java Runtime Environment"
+java --version
+echo "Java Development Kit"
+javac --version
+echo "GO"
+go version
+echo ""
+echo "LINUX SETUP ADDITIONAL STEPS..."
+echo "Settings -> Details -> Default Applications -> SET 'Music' & 'Video' TO 'VLC media player'"
+echo "Download & Install 'R Studio' @ https://www.rstudio.com/"
+echo "Download & Install 'Netbeans' @ https://netbeans.org/"
+echo "Download & Install 'XAMPP' @ https://www.apachefriends.org/"
+echo "Download & Install 'AdBlock' @ https://getadblock.com/"
