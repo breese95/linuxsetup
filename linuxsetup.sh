c02e8406b2c7168ae171592297583f4729e32ce4
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2019-08-14 14:43:06 -0400

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -25,39 +25,8 @@ apt install npm -y
 apt install snapd -y
 snap install go --classic
 snap install code --classic
-echo ""
-echo "PROGRAMS INSTALLED..."
-echo "Visual Studio Code"
-echo "VLC media player"
-echo "Docker"
-echo ""
-echo "PROGRAMMING LANGUAGES INSTALLED..."
-echo "Python"
-python --version
-echo "Python3"
-python3 --version
-echo "PHP"
-php -v
-echo "R"
-R --version
-echo "Java Runtime Environment"
-java --version
-echo "Java Development Kit"
-javac --version
-echo "NodeJS"
-nodejs --version
-echo "GO"
-/snap/bin/go version
-
 cat > /etc/environment << EOF1
 PATH="$PATH:/snap/bin"
 EOF1
-
 source /etc/environment
-echo ""
-echo "LINUX SETUP ADDITIONAL STEPS..."
-echo ""
-echo "RESTART COMPUTER TO INITALIZE ENVIROMENT PATH CHANGE"
-echo "Settings -> Details -> Default Applications -> SET 'Music' & 'Video' TO 'VLC media player'"
-echo "Download & Install 'R Studio' @ https://www.rstudio.com/"
-echo "Download & Install 'AdBlock' @ https://getadblock.com/"
+reboot
