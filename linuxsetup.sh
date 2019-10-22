dc1d79cbb3800168c14653a2ef4f04321c985939
PAUL GASS <38982740+paulgass@users.noreply.github.com>
2019-10-22 09:11:46 -0400

    Update linuxsetup.sh

--- linuxsetup.sh
+++ linuxsetup.sh
@@ -34,3 +34,14 @@ snap install code --classic
 export PATH=$PATH:/snap/bin
 
 reboot
+
+#code --install-extension ms-python.python --force --user-data-dir
+#code --install-extension felixfbecker.php-intellisense --force --user-data-dir
+#code --install-extension ms-vscode.cpptools --force --user-data-dir
+#code --install-extension ms-vscode.csharp --force --user-data-dir
+#code --install-extension ms-azuretools.vscode-docker --force --user-data-dir
+#code --install-extension redhat.java --force --user-data-dir
+#code --install-extension octref.vetur --force --user-data-dir
+#code --install-extension ms-vscode.go --force --user-data-dir
+#SET VLC AS DEFAULT AUDIO AND DEFAULT VIDEO
+#DOWNLOAD AND INSTALL RSTUDIO, JAVA RUNTIME ENVIRONEMNET, JAVA DEVELOPMENT KIT, NETBEANS, APACHE XAMPP
